59ab42d10264bf36e59ff45c094d63c58e3e0853
xxultragunnerxx <49918799+xxultragunnerxx@users.noreply.github.com>
2019-08-13 14:38:58 -0400

    cleaning up, still testing

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ function getStaticMap(userLocation) {
     "locations": userLocation,
     "size": "170,170"
   }
+
   const formattedStaticMapParams = formatUserParamsQuery(staticMapParams);
   const staticMapRequestURL = staticMapURL + formattedStaticMapParams
-
-
 
   fetch(staticMapRequestURL)
     .then(response => {
@@ -55,14 +54,20 @@ function getUserMapquestInfo(location) {
       throw new Error(response.statusText);
     })
     .then(responseJson => {
+
+      //checks the Json for what should be valid entries
+      if (responseJson.results[0].locations[0].adminArea3 === "" || responseJson.results[0].locations[0].adminArea4 === "") {
+        $('.error-messages').text('Please enter a valid location')
+        return $('#results-header').css('display', 'none');
+      }
       mapquestLat = `${responseJson.results[0].locations[0].latLng.lat}`
       mapquestLng = `${responseJson.results[0].locations[0].latLng.lng}`
       sunriseQuery(mapquestLat, mapquestLng, userDateSelected);
+      getStaticMap(responseJson.results[0].providedLocation.location);
     })
     .catch(err => {
       $('.search-results').txt(`${err.message}`)
     });
-
 }
 
 //makes the call to the sunrise api
@@ -97,10 +102,12 @@ function formatUserParamsQuery(mapquestObjectData) {
   return mapquestQueryItems.join('&');
 }
 
-
 //the results
 function sunriseResultsData(responseJSON) {
+
+  //makes date YYYY/MM/DD for iOS compatibility
   const newDate = Object.keys(responseJSON.results).map(key => `${new Date(`${userDateSelected.replace(/-/g, '/')} ${responseJSON.results[key]} UTC`)}`).map(string => string.slice(0, 24)).map(string => string.concat(` Local Time`))
+
   $('.search-results').append(
     `<div id="sunriseTime"><span>Sunrise: ${newDate[0]}<br>
   Sunset Time: ${newDate[1]}<br>
@@ -111,16 +118,13 @@ function sunriseResultsData(responseJSON) {
   Nautical Twilight End: ${newDate[7]}</span></div>
   <div id="astronomicalTime"><span>Astronomical Twilight Begin: ${newDate[8]}<br>
   Astronomical Twilight End: ${newDate[9]}</span></div>`).hide().fadeIn(1000)
-  //$('#sunriseTime, #civilTime, #nauticalTime, #astronomicalTime').fadeIn(1000);
 }
-
 
 //to draw map
 function drawMap(responseJson) {
   $('.map').empty()
   return $('.map').fadeIn(1000).append(`<img class="mapImg" src="${responseJson}" alt="picture of location">`).hide().fadeIn(1000);
 }
-
 
 //sets up event listeners
 function main() {
@@ -129,27 +133,23 @@ function main() {
     let userLocation = $('#user-input-location').val();
     userDateSelected = $('#user-input-date').val();
     getUserMapquestInfo(userLocation);
-    getStaticMap(userLocation);
     $('h1, fieldset, .tribute-text, .intro-text').fadeOut(250)
     $('#reset, #results-header').fadeIn(1000).css('display', 'block');
     return userDateSelected;
   })
 }
 
-$(main);
-
 //takes user back to start
 function reset() {
   $('.reset').on('click', '#reset', function () {
     $('#sunriseTime, #civilTime, #nauticalTime, #astronomicalTime, .mapImg, .results-header').remove();
-    //$('#sunriseTime, #civilTime, #nauticalTime, #astronomicalTime, .mapImg, .results-header').remove();
     $('h1, fieldset').fadeIn(1000);
-    //$('h1, fieldset').css('display','block');
-    //$('#reset',).fadeOut(250);
+    $('.error-messages').empty()
     $('#reset').css('display', 'none');
-    //$('#results-header').fadeOut(250);
     $('#results-header').css('display', 'none');
     $('.tribute-text, .intro-text').fadeIn(1000)
   })
 }
+
 $(reset);
+$(main);
